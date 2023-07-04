9da310567a91484f26e81415a81adab0ac845dcf
rudraGit234 <rudragit123@gmail.com>
2023-07-04 10:47:36 +0530

    final commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,14 +15,7 @@ router.get("/current", validateToken, currentUser)
 export default router
 
 
-// "_id": {
-//     "$oid": "64a2d369f6e37e1f408f8ee9"
-//   },
-//   "user_id": {
-//     "$oid": "64a2ceffd24cfd7c3ab8cf82"
 
-// 64a2ceffd24cfd7c3ab8cf82
-//   },
 
 
 
